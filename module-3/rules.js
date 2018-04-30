8c236e220503e885ab9058d5497d73c489834443
Steven Hicks <steven.j.hicks@gmail.com>
2018-04-29 23:55:39 -0500

    babel-preset-env just in case.

--- module-3/rules.js
+++ module-3/rules.js
@@ -1,6 +1,6 @@
 export default function doStuff(){
   const steve = {eyes: 'blue'};
-  const hicks = {...steve, hair: 'brown'};
+  const hicks = {...steve, hair: 'brown', height: '5 9'};
 
   console.log('yoooo', steve, hicks);
 }
