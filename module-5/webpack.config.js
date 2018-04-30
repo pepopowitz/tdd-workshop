5a6bc20afabac83a5e97e15b7e41c6e5dc7878c2
Steven Hicks <steven.j.hicks@gmail.com>
2018-04-29 23:56:30 -0500

    babel-preset-env for module 5.

--- module-5/webpack.config.js
+++ module-5/webpack.config.js
@@ -28,6 +28,16 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
-    ],
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env']
+          }
+        }
+      }
+    ]
   },
 };
