febec063a5e7ef10164573496abcdd38f233a6ab
Steven Hicks <steven.j.hicks@gmail.com>
2018-05-01 23:00:38 -0500

    20x20

--- module-3/get-next-state.spec.js
+++ module-3/get-next-state.spec.js
@@ -1,9 +1,10 @@
 import getNextState from './get-next-state';
 
 describe('module-3/get-next-state', () => {
-  it('works', () => {
-    const result = getNextState(1);
-    
-    expect(result.length).toBeGreaterThan(0);
+  it('returns a 20x20 grid', () => {
+    const result = getNextState();
+
+    expect(result.length).toEqual(20);
+    expect(result[0].length).toEqual(20);
   })
 });
